7b09f85890e5e55317f197f97f6b29c9931e9a28
André <imandreleandro@gmail.com>
2025-03-28 11:40:01 +0000

    abc

--- MyGame/Enemy.cs
+++ MyGame/Enemy.cs
@@ -3,10 +3,28 @@
     public class Enemy
     {
 
-        private string _name;
+        private string name;
+        public string GetName() => name;
+        private float health;
+        private float shield;
+        
         public Enemy(string name)
         {
-            _name = name;
+            this.name = name;
+            health = 100;
+            shield = 0;
+        }
+
+        public void TakeDamage(float damage)
+        {
+            shield -= damage;
+            if (shield < 0)
+            {
+                float damageLeft = -shield;
+                shield = 0;
+                health -= damageLeft;
+                if (health < 0) health = 0;
+            }
         }
         
     }

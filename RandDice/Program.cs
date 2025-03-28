0bcabd9198f2455b527227deb5d8faf42848bafb
André <imandreleandro@gmail.com>
2025-03-28 11:34:37 +0000

    Added RandDice project

---
+++ RandDice/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace RandDice
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Random rand = new Random(int.Parse(args[1]));
+            int total = 0;
+            for (int i = 0; i < int.Parse(args[0]); i++)
+            {
+                total += rand.Next(1, 7);
+            }
+            Console.WriteLine($"{total}");
+        }
+    }
+}

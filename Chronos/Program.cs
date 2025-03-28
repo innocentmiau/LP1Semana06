e5e2d89eb748a2b3ccc979ed7e5e98a807defdd1
André <imandreleandro@gmail.com>
2025-03-28 11:29:36 +0000

    completed Chronos project to showcase different stopwatch counters

--- Chronos/Program.cs
+++ Chronos/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Threading;
 
 namespace Chronos
 {
@@ -6,7 +8,18 @@ namespace Chronos
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            Stopwatch crono1 = new Stopwatch();
+            Stopwatch crono2 = new Stopwatch();
+            crono1.Start();
+            
+            Thread.Sleep(500);
+            crono2.Start();
+            Thread.Sleep(250);
+            crono1.Stop();
+            crono2.Stop();
+            
+            Console.WriteLine($"{(crono1.ElapsedMilliseconds/1000f):F4}s");
+            Console.WriteLine($"{(crono2.ElapsedMilliseconds/1000f):F4}s");
         }
     }
 }
